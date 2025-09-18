e824bc18a63896b777a888f0822c2ca45ea1d150
johnsenultra <johnsenultra446@gmail.com>
2025-09-19 04:58:45 +0800

    added my hero content

--- src/home/Hero.tsx
+++ src/home/Hero.tsx
@@ -1,3 +1,6 @@
+import DynamicText from "@/components/kokonutui/dynamic-text";
+import { Button } from "@/components/ui/button";
+
 export default function Hero() {
   return (
     <div className="relative h-full w-full bg-white">
@@ -12,14 +15,35 @@ export default function Hero() {
         }}
       />
       {/* Your Content/Components */}
-      <div className="relative z-10 flex min-h-56 border-5 border-dotted">
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium
-          ipsa quisquam a quia voluptatibus officia dignissimos molestiae
-          debitis nihil, amet natus. Minima harum ratione est inventore dolor
-          voluptates deleniti aliquid.
-        </p>
+      <div className="relative grow">
+        <section className="z-10 mx-auto mt-8 flex max-w-screen-xl flex-col items-center md:px-20">
+          <div className="my-4 w-[8rem] rounded-4xl bg-gray-900 text-center text-white">
+            <p>
+              <DynamicText />
+            </p>
+          </div>
+          <div className="mt-4 mb-10">
+            <h1 className="text-4xl font-[900] uppercase sm:text-5xl md:text-6xl lg:text-7xl">
+              J
+              <span className="bg-gradient-to-r from-black via-gray-700 to-black bg-clip-text text-transparent">
+                ohnsen Ultra
+              </span>
+            </h1>
+          </div>
+          <div className="max-w-7xl px-5 text-center text-sm text-gray-600 sm:text-xl md:px-0">
+            <p>
+              I’m a full-stack freelance software engineer specializing in
+              building high-quality custom web solutions, workflow automation,
+              and AI integrations.
+            </p>
+          </div>
+        </section>
+
+        <section className="mx-auto mt-9 max-w-screen-xl">
+          <div className="flex justify-center">
+            <Button className="text-sm font-bold">Download CV</Button>
+          </div>
+        </section>
       </div>
     </div>
   );
