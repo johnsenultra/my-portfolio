d3eb4e09c11c1a0ec6ab94cefef4fb225e42a96c
johnsenultra <johnsenultra446@gmail.com>
2025-09-20 01:18:04 +0800

    refactor the call to action label, and add add button for contact and download CV

--- src/home/Hero.tsx
+++ src/home/Hero.tsx
@@ -1,9 +1,10 @@
 import DynamicText from "@/components/kokonutui/dynamic-text";
 import { Button } from "@/components/ui/button";
+import { CloudDownload } from "lucide-react";
 
 export default function Hero() {
   return (
-    <div className="relative h-full w-full bg-white">
+    <div className="h-full w-full bg-white">
       {/* Amber Glow Background */}
       <div
         className="absolute inset-0 z-0"
@@ -16,11 +17,9 @@ export default function Hero() {
       />
       {/* Your Content/Components */}
       <div className="relative grow">
-        <section className="z-10 mx-auto mt-8 flex max-w-screen-xl flex-col items-center md:px-20">
+        <section className="z-10 mx-auto mt-8 flex max-w-screen-lg flex-col items-center md:px-20">
           <div className="my-4 w-[8rem] rounded-4xl bg-gray-900 text-center text-white">
-            <p>
-              <DynamicText />
-            </p>
+            <DynamicText />
           </div>
           <div className="mt-4 mb-10">
             <h1 className="text-4xl font-[900] uppercase sm:text-5xl md:text-6xl lg:text-7xl">
@@ -30,18 +29,28 @@ export default function Hero() {
               </span>
             </h1>
           </div>
-          <div className="max-w-7xl px-5 text-center text-sm text-gray-600 sm:text-xl md:px-0">
-            <p>
-              I’m a full-stack freelance software engineer specializing in
-              building high-quality custom web solutions, workflow automation,
-              and AI integrations.
+          <div className="max-w-7xl px-3 text-center md:px-0">
+            <p className="text-sm text-gray-600 sm:text-xl">
+              <span className="mb-1 inline-block w-4 border-b-2 border-gray-700"></span>{" "}
+              A freelancer and full stack developer specializing in building
+              high-quality custom web solutions, workflow automation, and AI
+              integrations.
             </p>
           </div>
         </section>
 
-        <section className="mx-auto mt-9 max-w-screen-xl">
-          <div className="flex justify-center">
-            <Button className="text-sm font-bold">Download CV</Button>
+        <section className="mx-auto mt-9 max-w-screen-lg">
+          <div className="mx-auto flex max-w-[45rem] justify-center gap-3 px-5">
+            <Button className="w-full rounded-4xl border border-gray-900 bg-black px-4 py-5 text-sm font-bold text-white transition-all duration-100 ease-in hover:border-gray-900 hover:bg-gray-900 focus:border-black focus:ring-black focus:outline-none md:w-[10rem]">
+              Contact Me
+            </Button>
+            <Button className="w-full rounded-4xl border border-gray-900 bg-transparent px-4 py-5 text-sm font-bold text-black transition-all duration-200 ease-in hover:border-gray-800 hover:bg-gray-200 hover:text-gray-800 focus:border-black focus:ring-black focus:outline-none md:w-[10rem]">
+              Download CV{" "}
+              <span className="ml-1">
+                {" "}
+                <CloudDownload />
+              </span>
+            </Button>
           </div>
         </section>
       </div>
