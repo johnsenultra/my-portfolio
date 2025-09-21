b335088f4d15b34b15f00213e741a84f405fae56
johnsenultra <johnsenultra446@gmail.com>
2025-09-22 01:08:59 +0800

    add Link from react-router-dom and, and add scroll animation

--- src/home/Hero.tsx
+++ src/home/Hero.tsx
@@ -1,6 +1,7 @@
 import DynamicText from "@/components/kokonutui/dynamic-text";
 import { Button } from "@/components/ui/button";
-import { CloudDownload } from "lucide-react";
+import { CloudDownload, HandshakeIcon } from "lucide-react";
+import { Link } from "react-router-dom";
 
 export default function Hero() {
   return (
@@ -16,7 +17,11 @@ export default function Hero() {
         }}
       />
       {/* Your Content/Components */}
-      <div className="relative grow">
+      <div
+        className="relative grow"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+      >
         <section className="z-10 mx-auto mt-8 flex max-w-screen-lg flex-col items-center md:px-20">
           <div className="my-4 w-[8rem] rounded-4xl bg-gray-900 text-center text-white">
             <DynamicText />
@@ -40,14 +45,18 @@ export default function Hero() {
         </section>
 
         <section className="mx-auto mt-9 max-w-screen-lg">
-          <div className="mx-auto flex max-w-[45rem] justify-center gap-3 px-5">
-            <Button className="w-full rounded-4xl border border-gray-900 bg-black px-4 py-5 text-sm font-bold text-white transition-all duration-100 ease-in hover:border-gray-900 hover:bg-gray-900 focus:border-black focus:ring-black focus:outline-none md:w-[10rem]">
-              Contact Me
-            </Button>
+          <div className="mx-auto flex max-w-[45rem] flex-col justify-center gap-4 px-5 md:flex-row">
+            <Link to="/message-me">
+              <Button className="w-full rounded-4xl border border-gray-900 bg-black px-4 py-5 text-sm font-bold text-white transition-all duration-100 ease-in hover:border-gray-900 hover:bg-gray-900 focus:border-black focus:ring-black focus:outline-none md:w-[10rem]">
+                Let’s Connect
+                <span className="ml-1">
+                  <HandshakeIcon />
+                </span>
+              </Button>
+            </Link>
             <Button className="w-full rounded-4xl border border-gray-900 bg-transparent px-4 py-5 text-sm font-bold text-black transition-all duration-200 ease-in hover:border-gray-800 hover:bg-gray-200 hover:text-gray-800 focus:border-black focus:ring-black focus:outline-none md:w-[10rem]">
-              Download CV{" "}
+              Download CV
               <span className="ml-1">
-                {" "}
                 <CloudDownload />
               </span>
             </Button>
