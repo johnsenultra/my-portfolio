0e4f9a1c425b8f06508162fda57a79856c326cdb
johnsenultra <johnsenultra446@gmail.com>
2025-09-22 01:01:19 +0800

    wrap the App with BrowserRouter, routes also implement AOS handling scrool amimation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./home/Landing";
+import About from "./home/About";
+import Layout from "./layout/Layout";
+import LetsConnect from "./home/LetsConnect";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1800,
+    });
+  }, []);
   return (
-    <>
-      <Landing />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Landing />} />
+          <Route path="about" element={<About />} />
+          <Route path="message-me" element={<LetsConnect />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
