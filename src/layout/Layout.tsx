5721518a222489e230b4cd0110ebcdb522639268
johnsenultra <johnsenultra446@gmail.com>
2025-10-02 11:29:22 +0800

    refactor: simplify Layout component by removing unnecessary background elements

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -3,23 +3,10 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="relative h-full w-full bg-white">
-      {/* Amber Glow Background */}
-      <div
-        className="absolute inset-0 z-0"
-        style={{
-          backgroundImage: `
-        radial-gradient(125% 125% at 50% 10%, #ffffff 40%, #f59e0b 100%)
-      `,
-          backgroundSize: "100% 100%",
-        }}
-      />
-      {/* Your Content/Components */}
-      <div className="flex min-h-screen flex-col">
-        <Navbar />
-        <div className="relative z-10 grow">
-          <Outlet />
-        </div>
+    <div className="flex min-h-screen flex-col">
+      <Navbar />
+      <div className="z-10 grow">
+        <Outlet />
       </div>
     </div>
   );
